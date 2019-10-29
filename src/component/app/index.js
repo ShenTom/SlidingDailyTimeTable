6433fab3087338e3463e324d56e146083fa42773
Tom Shen <tom@Toms-MacBook-Pro.local>
2019-10-29 19:04:18 -0400

    Add check for last wake up time

--- src/component/app/index.js
+++ src/component/app/index.js
@@ -2,21 +2,30 @@ import React from 'react';
 import WokeupButton from '../wokeupButton'
 import Timetable from '../timetable'
 import dateFormat from 'dateformat';
+import * as Constants from '../../constants'
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      wakeupTime: new Date()
+      wakeupTime: new Date(),
+      history: []
     }
   }
   
   buttonClick = () => {
     let d = new Date(Date())
-    this.setState({
-      wakeupTime: d
-    })
+    const last_date = this.state.history[this.state.history.length - 1]
+    const diff = (d - last_date)/(1000 * 60 * 60)
+    if (diff < Constants.WAKEUPINTERVAL){
+      console.log('Are you sure u woke up less than ' + Constants.WAKEUPINTERVAL + ' hours ago')
+    } else {
+      this.setState({
+        wakeupTime: d,
+        history: [...this.state.history, d]
+      })
+    }  
   }
   render() {
     console.log(this.state.wakeupTime)
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const WAKEUPINTERVAL = 2
